6378f9c4b4e164eecb426ca4e1d8689380627866
sara kalai <sara.kal2004@gmail.com>
2025-04-06 15:04:35 +0100

    start

---
+++ src/test/java/com/petconnect/petconnect/PetconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petconnect.petconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
